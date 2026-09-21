1effbe60a9cb51389f85b71c38242c587a713216
Patrick Massot <patrickmassot@free.fr>
2026-09-21 18:41:39 +0200

    Move lemma to match reorganization in master

--- Mathlib/LinearAlgebra/TensorProduct/Tower.lean
+++ Mathlib/LinearAlgebra/TensorProduct/Tower.lean
@@ -690,6 +690,11 @@ lemma baseChange_comp (g : N →ₗ[R] P) :
     (g ∘ₗ f).baseChange A = g.baseChange A ∘ₗ f.baseChange A := by
   ext; simp
 
+theorem rTensor_comp_baseChange_comm_apply
+    (φ : A →ₗ[R] B) (t : A ⊗[R] M) (f : M →ₗ[R] N) :
+    (φ.rTensor N) (f.baseChange A t)  = (f.baseChange B) (φ.rTensor M t) := by
+  simp [LinearMap.baseChange_eq_ltensor, ← LinearMap.comp_apply]
+
 open AlgebraTensorModule in
 lemma baseChange_baseChange {A B : Type*} [CommSemiring A] [Algebra R A]
     [Semiring B] [Algebra R B] [Algebra A B] [IsScalarTower R A B]
